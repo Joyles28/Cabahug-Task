d5c4f2251c1cb995f7eef462b280a2414ab23a65
Noel Jules Cabahug <njpcabahug@gmail.com>
2026-07-20 16:35:43 +0800

    Implement Task 2: Proper pricing with long-rental discount rule
    Total will be based on the number of days times the daily rate. The discount rule will be applied if the number of days reaches 7 or more, meaning if the number of days is 6. then there will be no discount, but if it reaches 7 or 8 or so on, then there is a 10% discount off the total.

--- app.py
+++ app.py
@@ -116,8 +116,10 @@ def calculate_total(daily_rate, days):
 
     TODO (Task 2): implement.
     """
-    raise NotImplementedError
-
+    total = days * daily_rate
+    if days >= 7:
+        total = total - (total * 0.1)
+    return total
 
 # ---------------------------------------------------------------------------
 # API
